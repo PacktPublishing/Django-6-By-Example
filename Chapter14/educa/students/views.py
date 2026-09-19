a483e4e4d002e3c195143eb0d1734fb6baa52460
Juliana Nicacio <juhnicacio.dev@gmail.com>
2026-09-19 15:33:18 +0200

    fix: use first() when retrieving course module

--- Chapter14/educa/students/views.py
+++ Chapter14/educa/students/views.py
@@ -63,5 +63,5 @@ class StudentCourseDetailView(LoginRequiredMixin, DetailView):
             context["module"] = course.modules.get(id=self.kwargs["module_id"])
         else:
             # get first module
-            context["module"] = course.modules.all()[0]
+            context["module"] = course.modules.first()
         return context
